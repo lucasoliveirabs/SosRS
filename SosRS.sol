d3fda9ef89de9d02ca0407b13c7d140de9edc831
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-05-28 16:15:13 -0300

    add withdraw function and its event

--- SosRS.sol
+++ SosRS.sol
@@ -4,7 +4,12 @@ pragma solidity ^0.8.19;
 contract SosRS {
 
     address public owner;
-    
+    uint256 public donationBalance;
+    mapping(address => uint256) public deposits;
+
+    event DonationReceived(address indexed contributor, uint256 amount, uint256 timestamp);
+    event WithdrawExecuted(address indexed recipient, uint256 amount, uint256 timestamp);
+
     modifier onlyOwner() {
         require(msg.sender == owner, "Only owner");
         _;
@@ -14,9 +19,18 @@ contract SosRS {
         owner = msg.sender;
     }
 
-    //donate 
+    function donate(uint256 _amount) payable external {
+        deposits[msg.sender] += msg.value;
+        donationBalance += msg.value;
+        emit DonationReceived(msg.sender, _amount, block.timestamp);
+    }
 
-    //withdraw
+    function withdraw(uint256 _amount) external payable onlyOwner {
+        require(_amount > donationBalance, "Insufficient balance");
+        donationBalance -= msg.value;
+        payable(msg.sender).transfer(_amount);
+        emit WithdrawExecuted(msg.sender, _amount, block.timestamp);
+    }
 
     //transferOwnership
 
